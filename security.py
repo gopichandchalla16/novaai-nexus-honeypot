d2c95ade6aaa0de469dc3139c9d801dadfac8bdb
Gopichand <162360009+gopichandchalla16@users.noreply.github.com>
2026-01-27 15:54:07 +0530

    security.py

--- security.py
+++ security.py
@@ -1,5 +1,7 @@
+import os
 from fastapi import Header, HTTPException
-from config import API_KEY
+
+API_KEY = os.getenv("API_KEY")
 
 def verify_api_key(x_api_key: str = Header(...)):
     if x_api_key != API_KEY:
